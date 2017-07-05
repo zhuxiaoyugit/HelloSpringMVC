95dd56d7b59c5e4d3c6c6844dd5d9581a9be4ffe
zhuguangyu <zhuguangyu@imohoo.com>
2017-07-05 17:08:52 +0800

    commit

---
+++ HelloSpring-MVC/src/main/java/com/yiibai/tutorial/springmvc/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.yiibai.tutorial.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+ 
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+         
+        model.addAttribute("greeting", "Hello Spring MVC");
+         
+        return"helloworld";
+         
+    }
+ 
+}
